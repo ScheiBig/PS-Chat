0b5b2ae0387b04cf71d0c3469a3368a06b72bb53
Marcin Jeznach <scheissig@gmail.com>
2019-08-05 14:11:10 +0200

    ChatMessage changed from interface to abstract class, factory parser moved to companion

--- src/main/kotlin/ScheiBig/PS/UDPChat/Messages/ChatMessage.kt
+++ src/main/kotlin/ScheiBig/PS/UDPChat/Messages/ChatMessage.kt
@@ -1,47 +1,52 @@
 package ScheiBig.PS.UDPChat.Messages
 
-interface ChatMessage {
+abstract class ChatMessage {
 
     val length: Int
         get() = toString().length
 
-    override fun toString(): String
+    abstract override fun toString(): String
 
-    @Throws(java.lang.IllegalArgumentException::class)
-    fun parseMessage(message: String): ChatMessage {
+    companion object {
+        @JvmStatic
+        @Throws(java.lang.IllegalArgumentException::class)
+        fun parseMessage(message: String): ChatMessage {
 
-        val badNumberParametersEx = IllegalArgumentException("Wrong number of parameters!")
-        val unknownParametersException = java.lang.IllegalArgumentException("Unknown message type")
+            val badNumberParametersEx = IllegalArgumentException("Wrong number of parameters!")
+            val unknownParametersException = java.lang.IllegalArgumentException("Unknown message type")
 
-        val splitMessage = message.split(Regex(" "))
-        when (splitMessage.first()) {
-            "NICK" -> {
-                return if (splitMessage.last() == "BUSY") {
-                    if (splitMessage.size != 3) throw badNumberParametersEx
-                    BusyNickChatMessage(splitMessage[1])
-                } else {
-                    if (splitMessage.size != 2) throw badNumberParametersEx
-                    SetNickChatMessage(splitMessage[1])
+            val splitMessage = message.split(Regex(" "))
+            when (splitMessage.first()) {
+                "NICK" -> {
+                    return if (splitMessage.last() == "BUSY") {
+                        if (splitMessage.size != 3) throw badNumberParametersEx
+                        BusyNickChatMessage(splitMessage[1])
+                    } else {
+                        if (splitMessage.size != 2) throw badNumberParametersEx
+                        SetNickChatMessage(splitMessage[1])
+                    }
                 }
+                "JOIN" -> {
+                    if (splitMessage.size != 3) throw badNumberParametersEx
+                    return JoinRoomNickChatMessage(splitMessage[1], splitMessage[2])
+                }
+                "LEAVE" -> {
+                    if (splitMessage.size != 3) throw badNumberParametersEx
+                    return JoinRoomNickChatMessage(splitMessage[1], splitMessage[2])
+                }
+                "MSG" -> {
+                    if (splitMessage.size <= 3) throw badNumberParametersEx
+                    return MessageRoomNickChatMessage(
+                        splitMessage[1], splitMessage[2],
+                        splitMessage.subList(3, splitMessage.lastIndex).joinToString(" ")
+                    )
+                }
+                "WHOIS" -> {
+                    if (splitMessage.size != 2) throw badNumberParametersEx
+                    return WhoisRoomChatMessage(splitMessage[1])
+                }
+                else -> throw unknownParametersException
             }
-            "JOIN" -> {
-                if (splitMessage.size != 3) throw badNumberParametersEx
-                return JoinRoomNickChatMessage(splitMessage[1], splitMessage[2])
-            }
-            "LEAVE" -> {
-                if (splitMessage.size != 3) throw badNumberParametersEx
-                return JoinRoomNickChatMessage(splitMessage[1], splitMessage[2])
-            }
-            "MSG" -> {
-                if (splitMessage.size <= 3) throw badNumberParametersEx
-                return MessageRoomNickChatMessage(splitMessage[1], splitMessage[2],
-                    splitMessage.subList(3, splitMessage.lastIndex).joinToString(" "))
-            }
-            "WHOIS" -> {
-                if (splitMessage.size != 2) throw badNumberParametersEx
-                return WhoisRoomChatMessage(splitMessage[1])
-            }
-            else -> throw unknownParametersException
         }
     }
 }
